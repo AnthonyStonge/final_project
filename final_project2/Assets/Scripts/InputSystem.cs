b3f671adbae3e2e2704980055068c5b21cfea5fe
astonge <astonge@isi-mtl.com>
2020-04-01 00:18:54 -0400

    Basic Project With package

---
+++ final_project2/Assets/Scripts/InputSystem.cs
@@ -0,0 +1,29 @@
+﻿using Unity.Burst;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Jobs;
+using Unity.Mathematics;
+using Unity.Transforms;
+using UnityEngine;
+using static Unity.Mathematics.math;
+
+[AlwaysUpdateSystem]
+public class InputSystem : SystemBase
+{
+    protected override void OnUpdate()
+     {
+         bool interact = false;
+             
+         if (Input.GetKeyDown(KeyCode.Space))
+         {
+             Debug.Log("Spacebar");
+             interact = true;
+         }
+ 
+         if (Input.GetKeyUp(KeyCode.Space))
+         {
+             interact = false;
+         }
+         Entities.ForEach((ref InputComponent ic) => { ic.interact = interact; }).Schedule();
+     }
+ }
---
+++ final_project2/Assets/Scripts/PhysicsBallsDeepSystem.cs
@@ -0,0 +1,32 @@
+﻿using Unity.Burst;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Jobs;
+using Unity.Mathematics;
+using Unity.Transforms;
+using Unity.Physics;
+using UnityEngine;
+using static Unity.Mathematics.math;
+
+public class PhysicsBallsDeepSystem : SystemBase
+{
+    protected override void OnCreate()
+    {
+    }
+    
+    protected override void OnStartRunning()
+    {
+    }
+
+    protected override void OnDestroy()
+    {
+    }
+
+    protected override void OnUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            Debug.Log("WHATATSTSTSTSST");
+        }
+    }
+}
