903db1a348068007be4de906ee5ec3a22ab30aaa
Gabriel Alexandre Desgagné <gdesgagn@isimtl.local>
2020-04-06 21:10:39 -0400

    System Dash + TimeDecrement + Component Dash

--- final_project4/Assets/Scripts/Components/DashComponent.cs
+++ final_project4/Assets/Scripts/Components/DashComponent.cs
@@ -1,18 +1,9 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using System;
+using Unity.Entities;
 
-public class DashComponent : MonoBehaviour
+[Serializable]
+public struct DashComponent : IComponentData
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public TimeTrackerComponent Timer;
+    public float Distance;
 }
--- final_project4/Assets/Scripts/Systems/Simulation/BeforeTransformSystemGroup/StateDyingSystem.cs
+++ final_project4/Assets/Scripts/Systems/Simulation/BeforeTransformSystemGroup/StateDyingSystem.cs
@@ -1,5 +1,4 @@
 ﻿using Unity.Entities;
-using UnityEngine;
 
 public class StateDyingSystem : SystemBase
 {
