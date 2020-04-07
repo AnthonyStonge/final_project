fa6963914afa6538c176a7acdbc3dab99a4b6a3f
zammorrak <46572000+zammorrak@users.noreply.github.com>
2020-04-07 09:38:41 -0400

    #120 PlayerRotationSystem
    Made system for rotation to target

--- final_project4/Assets/Scripts/Systems/Simulation/TransformSystemGroup/RotatePlayerSystem.cs
+++ final_project4/Assets/Scripts/Systems/Simulation/TransformSystemGroup/RotatePlayerSystem.cs
@@ -1,18 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
 using UnityEngine;
-
-public class RotatePlayerSystem : MonoBehaviour
+public class RotatePlayerSystem : SystemBase
 {
-    // Start is called before the first frame update
-    void Start()
+    protected override void OnUpdate()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Entities.ForEach((Entity e, ref Rotation rotation, ref TargetData target, ref PlayerTag playerTag, ref Translation translation) =>
+            {
+                Vector3 relativePos = new Vector3(target.Value.x, target.Value.y, target.Value.z) - new Vector3(translation.Value.x, translation.Value.y, translation.Value.z);
+                rotation.Value = quaternion.LookRotation(relativePos, Vector3.up);
+            }).Schedule();
     }
 }
