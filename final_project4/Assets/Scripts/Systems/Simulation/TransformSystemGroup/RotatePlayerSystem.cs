5842d38c51445e2d01389dbd48847e63c93ffa1a
astonge <astonge@isi-mtl.com>
2020-04-08 09:12:58 -0400

    Small bug fix

--- final_project4/Assets/Scripts/Systems/Simulation/TransformSystemGroup/RotatePlayerSystem.cs
+++ final_project4/Assets/Scripts/Systems/Simulation/TransformSystemGroup/RotatePlayerSystem.cs
@@ -14,8 +14,8 @@ public class RotatePlayerSystem : SystemBase
         Entities.WithAll<PlayerTag>().ForEach((ref Rotation rotation, in TargetData target, in Translation translation) =>
         {
             float3 forward = target.Value - translation.Value;
-                quaternion rot = quaternion.LookRotation(forward, math.up());
-                rotation.Value = math.normalize(new quaternion(0, rot.value.y, 0, rot.value.w));
+            quaternion rot = quaternion.LookRotation(forward, math.up());
+            rotation.Value = math.normalize(new quaternion(0, rot.value.y, 0, rot.value.w));
         }).Schedule();
     }
 }
