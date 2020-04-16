4fca49520b94e440f743c92d61e0b6bbcf6550f1
Marc-Antoine Girard <m.antoine.girard@gmail.com>
2020-04-16 13:53:41 -0400

    Demo on Cube Prefab

---
+++ final_project4/Assets/Prefabs/asdasd.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+using Unity.Entities;
+using Unity.Mathematics;
+using UnityEngine;
+
+[DisallowMultipleComponent]
+[RequiresEntityConversion]
+public class asdasd : MonoBehaviour, IConvertGameObjectToEntity
+{
+    public void Convert(Entity entity, EntityManager dstManager, GameObjectConversionSystem conversionSystem)
+    {
+    }
+}
+
