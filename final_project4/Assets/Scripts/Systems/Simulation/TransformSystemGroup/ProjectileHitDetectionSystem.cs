55a8ad3e59aac773f21d37dfe21b0ef7497c4002
astonge <astonge@isi-mtl.com>
2020-04-18 16:16:28 -0400

    Removed unused synch point

--- final_project4/Assets/Scripts/Systems/Simulation/TransformSystemGroup/ProjectileHitDetectionSystem.cs
+++ final_project4/Assets/Scripts/Systems/Simulation/TransformSystemGroup/ProjectileHitDetectionSystem.cs
@@ -1,13 +1,11 @@
 ﻿using Unity.Collections;
 using Unity.Entities;
-using Unity.Jobs;
 using Unity.Mathematics;
 using Unity.Physics;
 using Unity.Physics.Systems;
 using Unity.Transforms;
 using RaycastHit = Unity.Physics.RaycastHit;
 
-//[UpdateAfter(typeof(BuildPhysicsWorld))]
 [UpdateAfter(typeof(EndFramePhysicsSystem))]
 public class ProjectileHitDetectionSystem : SystemBase
 {
