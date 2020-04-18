90effb3ffaa842a55ab6a4af2c2cef3694a183e6
astonge <astonge@isi-mtl.com>
2020-04-18 16:15:03 -0400

    Bug fix on bullet collision hit

--- final_project4/Assets/Scripts/Systems/Simulation/TransformSystemGroup/ProjectileHitDetectionSystem.cs
+++ final_project4/Assets/Scripts/Systems/Simulation/TransformSystemGroup/ProjectileHitDetectionSystem.cs
@@ -7,43 +7,41 @@ using Unity.Physics.Systems;
 using Unity.Transforms;
 using RaycastHit = Unity.Physics.RaycastHit;
 
-[UpdateAfter(typeof(BuildPhysicsWorld))]
-[UpdateBefore(typeof(EndFramePhysicsSystem))]
-
+//[UpdateAfter(typeof(BuildPhysicsWorld))]
+[UpdateAfter(typeof(EndFramePhysicsSystem))]
 public class ProjectileHitDetectionSystem : SystemBase
 {
     private BuildPhysicsWorld physicsWorld;
-    private PreTransformGroupBarrier preTransformBarrier;
-    
+    private EndSimulationEntityCommandBufferSystem preTransformBarrier;
+
     protected override void OnCreate()
     {
         base.OnCreate();
 
-        preTransformBarrier = World.GetOrCreateSystem<PreTransformGroupBarrier>();
+        preTransformBarrier = World.GetOrCreateSystem<EndSimulationEntityCommandBufferSystem>();
         physicsWorld = World.GetOrCreateSystem<BuildPhysicsWorld>();
-        
     }
 
     protected override void OnUpdate()
     {
-         PhysicsWorld PhysicsWorld = physicsWorld.PhysicsWorld;
-         var entityCommandBuffer = preTransformBarrier.CreateCommandBuffer().ToConcurrent();
-         
-         //Get all enemy existing
-         ComponentDataContainer<EnemyTag> enemies = new ComponentDataContainer<EnemyTag>
-         {
-             Components = GetComponentDataFromEntity<EnemyTag>()
-         }; 
+        PhysicsWorld PhysicsWorld = physicsWorld.PhysicsWorld;
+        var entityCommandBuffer = preTransformBarrier.CreateCommandBuffer().ToConcurrent();
 
-         float deltaTime = Time.DeltaTime;
-         
-         CollisionFilter filter = new CollisionFilter
-         {
-             BelongsTo = 1 << 0,
-             CollidesWith = 1 << 10 | 1 << 2,
-             GroupIndex = 0
-         };
-         
+        //Get all enemy existing
+        ComponentDataContainer<EnemyTag> enemies = new ComponentDataContainer<EnemyTag>
+        {
+            Components = GetComponentDataFromEntity<EnemyTag>()
+        };
+
+        float deltaTime = Time.DeltaTime;
+
+        CollisionFilter filter = new CollisionFilter
+        {
+            BelongsTo = 1 << 0,
+            CollidesWith = 1 << 10 | 1 << 2,
+            GroupIndex = 0
+        };
+        
         Entities.ForEach((Entity entity, int entityInQueryIndex, ref DamageProjectile projectile, in Translation translation, in Rotation rotation) =>
         {
             //System.IndexOutOfRangeException: Index {0} is out of range of '{4}' Length.
@@ -83,9 +81,8 @@ public class ProjectileHitDetectionSystem : SystemBase
             }
             raycastHits.Dispose();
         }).ScheduleParallel();
-        
-        preTransformBarrier.AddJobHandleForProducer(Dependency);
 
+        preTransformBarrier.AddJobHandleForProducer(Dependency);
     }
 }
 
