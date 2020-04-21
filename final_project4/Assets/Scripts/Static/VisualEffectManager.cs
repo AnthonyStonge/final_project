c88ad2113db0d7090e5f9259dd97a15116c4f5bb
Gabriel Alexandre Desgagné <gdesgagn@isimtl.local>
2020-04-21 11:09:46 -0400

    #141 Created classes

---
+++ final_project4/Assets/Scripts/Static/VisualEffectManager.cs
@@ -0,0 +1,13 @@
+﻿using Enums;
+using Unity.Mathematics;
+using UnityEngine;
+using UnityEngine.VFX;
+
+public static class VisualEffectManager 
+{
+    public static void PlayVisualEffect(VFXType type, float3 position, quaternion rotation)
+    {
+        
+        
+    }
+}
---
+++ final_project4/Assets/Scripts/Systems/RetrieveVisualEventSystem.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Entities;
+using UnityEngine;
+
+[UpdateAfter(typeof(ProjectileHitDetectionSystem))]
+public class RetrieveVisualEventSystem : SystemBase
+{
+    
+    
+    protected override void OnUpdate()
+    {
+        //Take event and play it at desired position
+        //Bullets
+    }
+}
