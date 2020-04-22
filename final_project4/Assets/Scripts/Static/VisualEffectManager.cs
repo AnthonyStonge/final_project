8b6c8c797643f0d9706653a78023e0e4694dfd3c
Gabriel Alexandre Desgagné <gdesgagn@isimtl.local>
2020-04-21 23:20:34 -0400

    #141 Basic Functions

--- final_project4/Assets/Scripts/Static/VisualEffectManager.cs
+++ final_project4/Assets/Scripts/Static/VisualEffectManager.cs
@@ -1,14 +1,28 @@
-﻿using EventStruct;
+﻿using System;
+using EventStruct;
+using Unity.Mathematics;
+using UnityEngine;
+using UnityEngine.VFX;
+using UnityEngine.VFX.Utility;
 
-public static class VisualEffectManager 
+public static class VisualEffectManager
 {
     public static void PlayVisualEffect(BulletInfo info)
     {
-        
+        //Get visual effect from object pool
     }
-    
+
     public static void PlayVisualEffect(WeaponInfo info)
     {
-        
+        //Get visual effect from object pool
+    }
+
+    private static void InitializeAndPlay(float3 position, quaternion rotation)
+    {
+        //Set info
+
+        //Add event to OnEndEvent
+
+        //Play
     }
 }
--- final_project4/Assets/Scripts/Systems/RetrieveVisualEventSystem.cs
+++ final_project4/Assets/Scripts/Systems/RetrieveVisualEventSystem.cs
@@ -7,17 +7,16 @@ using UnityEngine;
 [UpdateAfter(typeof(ProjectileHitDetectionSystem))]
 public class RetrieveVisualEventSystem : SystemBase
 {
-    
-    
     protected override void OnUpdate()
     {
         //Take event and play it at desired position
         //Weapons
         foreach (WeaponInfo info in EventsHolder.WeaponEvents)
         {
-            VisualEffectManager.PlayVisualEffect(info);
+            if (info.EventType == WeaponInfo.WeaponEventType.ON_SHOOT)
+                VisualEffectManager.PlayVisualEffect(info);
         }
-        
+
         //Bullets
         foreach (BulletInfo info in EventsHolder.BulletsEvents)
         {
