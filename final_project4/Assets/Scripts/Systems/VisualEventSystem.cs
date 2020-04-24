9d0f96f7a9fed30ae6f1ff7a02f4b6968f9e993b
Marc-Antoine Girard <m.antoine.girard@gmail.com>
2020-04-24 17:22:53 -0400

    Optimize VFX position set

--- final_project4/Assets/Scripts/Systems/VisualEventSystem.cs
+++ final_project4/Assets/Scripts/Systems/VisualEventSystem.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using Enums;
 using EventStruct;
+using Unity.Collections;
 using Unity.Entities;
 using Unity.Mathematics;
 using UnityEngine;
@@ -16,47 +17,65 @@ public class VisualEventSystem : SystemBase
 
     private int propertyTexture;
     private int propertyCount;
-
+    private NativeList<BulletInfo> bulletsEvents;
     private VisualEffect visualEffect;
     protected override void OnCreate()
     {
-        //Might need to initialize it
+        //Caching BulletsEvents Reference
+        bulletsEvents = EventsHolder.BulletsEvents;
+        bulletsEvents.Clear();
+        
+        //Init position array
         colorPositions = new Color[2048];
+        
+        //Get Property ID
         propertyTexture = VisualEffectHolder.PropertyTexture;
         propertyCount = VisualEffectHolder.PropertyCount;
+        
+        /*
+        Create texture. Will only take 2048 positions,
+        texture RGBAFloat is the key here. It allows Colors to have any values.
+        */
         texture = new Texture2D(2048, 1, TextureFormat.RGBAFloat, false);
         visualEffect = VisualEffectHolder.ProjectileVFXDict[ProjectileType.PistolBullet];
-        EventsHolder.BulletsEvents.Clear();
+        
+        //Since Texture is a reference type, it can be linked once in the shader
+        visualEffect.SetTexture(propertyTexture, texture);
+        texture.name = "Bullets";
     }
 
     protected override void OnUpdate()
     {
-        var count = EventsHolder.BulletsEvents.Length;
+        //Get Event Count
+        var count = bulletsEvents.Length;
+        
+        //Tell the Visual Effect
+        visualEffect.SetInt(propertyCount, count);
 
-        Debug.Log(count);
-        // Takes only 2048 event from the last one
+        //Returns if there's no event
+        if (count == 0) return;
+        
+        // Takes only the 2048 event (max) from the last one
         var min = math.clamp(count - 2048, 0, count);
         
         for (var i = min; i < count; i++)
         {
-            //Could cache EventsHolder maybe?
-            float3 position = EventsHolder.BulletsEvents[i].HitPosition;
-            Color color = colorPositions[i];
+            var position = bulletsEvents[i].HitPosition;
+            var color = colorPositions[i];
             color.r = position.x;
             color.g = position.y;
             color.b = position.z;
-            
-            //Note https://answers.unity.com/questions/266170/for-different-texture-sizes-which-is-faster-setpix.html
-            // Says that setPixels32 is 15x faster than setPixel
-            texture.SetPixel(i, 0, new Color(position.x,position.y, position.z));
+            colorPositions[i] = color;
         }
+        //SetPixels is up to 3x faster then SetPixel
+        texture.SetPixels(0, 0, count, 1, colorPositions);
         texture.Apply();
 
-        visualEffect.SetTexture(propertyTexture, texture);
         visualEffect.SetInt(propertyCount, count);
         visualEffect.Play();
-        EventsHolder.BulletsEvents.Clear();
-
+        
+        //TODO Clear events all at the same place
+        bulletsEvents.Clear();
     }
 
     protected override void OnDestroy()
