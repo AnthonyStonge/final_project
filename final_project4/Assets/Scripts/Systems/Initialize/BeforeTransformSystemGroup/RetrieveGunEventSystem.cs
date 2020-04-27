9c320ff0e67b81b873a364884d93a383bd9c739e
Gabriel Alexandre Desgagné <gdesgagn@isimtl.local>
2020-04-27 13:39:43 -0400

    Micro Minor Optimisation

--- final_project4/Assets/Scripts/Systems/Initialize/BeforeTransformSystemGroup/RetrieveGunEventSystem.cs
+++ final_project4/Assets/Scripts/Systems/Initialize/BeforeTransformSystemGroup/RetrieveGunEventSystem.cs
@@ -56,7 +56,7 @@ public class RetrieveGunEventSystem : SystemBase
         float deltaTime = Time.DeltaTime;
 
         JobHandle gunJob = Entities.ForEach(
-            (Entity e, int entityInQueryIndex, ref GunComponent gun, ref LocalToWorld transform, in Parent parent) =>
+            (Entity e, int entityInQueryIndex, ref GunComponent gun, in LocalToWorld transform, in Parent parent) =>
             {
                 //Make sure gun has a parent
                 if (!states.Components.HasComponent(parent.Value))
