537a1b279b7ef1b427fedf4f7d7231abb060518a
Marc-Antoine Girard <m.antoine.girard@gmail.com>
2020-04-30 20:51:37 -0400

    #84 Player Animation

---
+++ final_project4/Assets/Editor/RangeDrawer.cs
@@ -0,0 +1,62 @@
+﻿using UnityEditor;
+using UnityEngine;
+namespace DefaultNamespace
+{
+    [CustomPropertyDrawer(typeof(Range))]
+    public class RangeDrawer : PropertyDrawer
+    {
+            SerializedProperty min, max, value;
+            string name;
+        
+            public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+            {
+                name = property.displayName;
+                min = property.FindPropertyRelative("Min");
+                max = property.FindPropertyRelative("Max");
+                value = property.FindPropertyRelative("value");
+        
+                Rect contentPosition = EditorGUI.PrefixLabel(position, new GUIContent(name));
+                float half = contentPosition.width / 2;
+                GUI.skin.label.padding = new RectOffset(3, 3, 6, 6);
+        
+                //show the Min and Max from the Range
+                EditorGUIUtility.labelWidth = 24f;
+                contentPosition.width *= 0.5f;
+                contentPosition.height = 16f;
+                EditorGUI.indentLevel = 0;
+        
+                // Draw min value
+                EditorGUI.BeginProperty(contentPosition, label, min);
+                {
+                    min.floatValue  = EditorGUI.FloatField(contentPosition, new GUIContent("min"), min.floatValue);
+                }
+                EditorGUI.EndProperty();
+        
+                contentPosition.x += half;
+                
+                //Draw Max value
+                EditorGUI.BeginProperty(contentPosition, label, max);
+                {
+                    max.floatValue = EditorGUI.FloatField(contentPosition, new GUIContent("max"), max.floatValue);
+                }
+                EditorGUI.EndProperty();
+                
+                contentPosition.y += 16f;
+                contentPosition.x -= half;
+                contentPosition.width *= 2f;
+                
+                //Draw current value
+                EditorGUI.BeginProperty(contentPosition, label, max);
+                {
+                    value.floatValue = EditorGUI.Slider(contentPosition, value.floatValue, min.floatValue, max.floatValue);
+                }
+                EditorGUI.EndProperty();
+                
+            }
+        
+            public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+            {
+                return 32f;
+            }
+    }
+}
--- final_project4/Assets/Scripts/Components/AnimationData.cs
+++ final_project4/Assets/Scripts/Components/AnimationData.cs
@@ -8,5 +8,22 @@ using UnityEngine;
 
 public struct AnimationData : IComponentData
 {
-    public ushort MeshIndexAt;
+    public short MeshIndexAt;
+    public bool Decrement;
+    public short MaxIndex;
+
+
+    public void ChangeFrame()
+    {
+        if (Decrement)
+        {
+            MeshIndexAt--;
+            if (MeshIndexAt < 0) MeshIndexAt = MaxIndex;
+        }
+        else
+        {
+            MeshIndexAt++;
+            if (MeshIndexAt > MaxIndex) MeshIndexAt = 0;
+        }
+    }
 }
