591fca35e5559b167e396868389a5c79c5227843
zammorrak <46572000+zammorrak@users.noreply.github.com>
2020-05-03 21:08:27 -0400

    #176 Begin Spawn
    Can put spawn on grid and preSpawn

--- final_project4/Assets/Scripts/ScriptableObjectScripts/ScriptableGrid.cs
+++ final_project4/Assets/Scripts/ScriptableObjectScripts/ScriptableGrid.cs
@@ -9,4 +9,10 @@ public class ScriptableGrid : ScriptableObject
     public int2 gridSize;
     public float3 nodeSize;
     public List<int> indexNoWalkable = new List<int>();
+    [HideInInspector]
+    public List<int> enemyPreSpawnerIndex = new List<int>();
+    [HideInInspector]
+    public List<int> enemySpawnerIndex = new List<int>();
+    public List<EnemyPreSpawnerInfo> enemyPreSpawner = new List<EnemyPreSpawnerInfo>();
+    public List<EnemySpawnerInfo> enemySpawner = new List<EnemySpawnerInfo>();
 }
---
+++ final_project4/Assets/Scripts/Struct/EnemySpawnerInfo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Entities;
+using Unity.Mathematics;
+using UnityEngine;
+using Type = Enums.Type;
+[Serializable]
+public struct EnemySpawnerInfo
+{
+    public int2 spawnerPos;
+    public int NbEnnemyMax;
+    public Type EnemyType;
+    public int currentEnnemySpawn;
+    public int2 TimeRangeBetweenSpawn;
+    [HideInInspector]
+    public float currentTime;
+    [HideInInspector]
+    public float time;
+}
