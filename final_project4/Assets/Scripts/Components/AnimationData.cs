6b164d2f84580d7e8953830bb33b336b6841eaef
Marc-Antoine Girard <m.antoine.girard@gmail.com>
2020-05-06 22:10:18 -0400

    #54 #53 Goriillaaaaaaaaz

--- final_project4/Assets/Scripts/Components/AnimationData.cs
+++ final_project4/Assets/Scripts/Components/AnimationData.cs
@@ -9,21 +9,4 @@ using UnityEngine;
 public struct AnimationData : IComponentData
 {
     public short MeshIndexAt;
-    public bool Decrement;
-    public short MaxIndex;
-
-
-    public void ChangeFrame()
-    {
-        if (Decrement)
-        {
-            MeshIndexAt--;
-            if (MeshIndexAt < 0) MeshIndexAt = MaxIndex;
-        }
-        else
-        {
-            MeshIndexAt++;
-            if (MeshIndexAt > MaxIndex) MeshIndexAt = 0;
-        }
-    }
 }
